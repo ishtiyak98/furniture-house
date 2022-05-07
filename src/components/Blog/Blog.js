150a4d35afe35ef943879c0609de7ba094d310e7
ishtiyak98 <rifatishtiyak@gmail.com>
2022-05-07 22:46:37 +0600

    blog answer added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -10,31 +10,53 @@ const Blog = () => {
         <h1 className="text-center fw-bold mb-4">My Blogs</h1>
 
         <div className="item-card p-4 rounded-3 bg-black mb-4">
-            <h3 className="mb-3 text-white">Difference between javascript and nodejs</h3>
-            <p className="text-white">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex animi maiores odit commodi necessitatibus laudantium ipsam! Cumque beatae deserunt non sed asperiores et, repellat repellendus consectetur quis, libero quibusdam similique ea, vel neque rem blanditiis soluta quaerat ab dolore possimus modi ducimus alias temporibus. Unde fuga velit delectus rem necessitatibus. Reiciendis laborum optio doloremque, repudiandae sapiente pariatur aliquam fugit distinctio obcaecati maxime eum. Illo eum quia provident nihil voluptatibus. Asperiores delectus fugit repudiandae corrupti? Adipisci assumenda porro odio ullam ducimus exercitationem provident deserunt explicabo nam omnis eveniet suscipit culpa dolores, voluptate amet, nisi voluptas? Id nemo aperiam non saepe itaque!
-            </p>
+          <h3 className="mb-3 text-white">
+            Difference between JavaScript and NodeJS
+          </h3>
+          <p className="text-white">
+            JavaScript is a programming language and NodeJS is not a programming
+            language. NodeJS is a runtime environment for JavaScript. We can
+            only run JavaScript in browsers but if we want to run JavaScript
+            outside the browser then we have to use NodeJS. We use JavaScript on
+            the client-side whereas Nodejs is used on the server-side.
+            JavaScript can run in any browser engine and NodeJS uses the V8
+            engine to run JavaScript. Adding HTML and DOM manipulation can be
+            done using JavaScript whereas NodeJS cannot add HTML tags and DOM
+            manipulation.
+          </p>
         </div>
 
         <div className="item-card p-4 rounded-3 bg-black mb-4 ">
-            <h3 className="mb-3 text-white">When should you use nodejs and when should you use mongodb?</h3>
-            <p className="text-white">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex animi maiores odit commodi necessitatibus laudantium ipsam! Cumque beatae deserunt non sed asperiores et, repellat repellendus consectetur quis, libero quibusdam similique ea, vel neque rem blanditiis soluta quaerat ab dolore possimus modi ducimus alias temporibus. Unde fuga velit delectus rem necessitatibus. Reiciendis laborum optio doloremque, repudiandae sapiente pariatur aliquam fugit distinctio obcaecati maxime eum. Illo eum quia provident nihil voluptatibus. Asperiores delectus fugit repudiandae corrupti? Adipisci assumenda porro odio ullam ducimus exercitationem provident deserunt explicabo nam omnis eveniet suscipit culpa dolores, voluptate amet, nisi voluptas? Id nemo aperiam non saepe itaque!
-            </p>
+          <h3 className="mb-3 text-white">
+            Differences between SQL and NoSQL databases?
+          </h3>
+          <p className="text-white">
+            SQL is a Structured Query language and it is a Relational Database
+            Management System (RDBMS). NoSQL is a non-relational or distributed
+            database system. The structure of the SQL database is a table-based
+            system whereas NoSQL databases use key-value pairs document-based
+            systems. SQL database is vertically scalable and NoSQL database is
+            horizontally scalable. SQL is not suitable for hierarchical data
+            storage but NoSQL is best suited for hierarchical data storage.
+          </p>
         </div>
 
         <div className="item-card p-4 rounded-3 bg-black mb-4 ">
-            <h3 className="mb-3 text-white">Differences between sql and nosql databases?</h3>
-            <p className="text-white">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex animi maiores odit commodi necessitatibus laudantium ipsam! Cumque beatae deserunt non sed asperiores et, repellat repellendus consectetur quis, libero quibusdam similique ea, vel neque rem blanditiis soluta quaerat ab dolore possimus modi ducimus alias temporibus. Unde fuga velit delectus rem necessitatibus. Reiciendis laborum optio doloremque, repudiandae sapiente pariatur aliquam fugit distinctio obcaecati maxime eum. Illo eum quia provident nihil voluptatibus. Asperiores delectus fugit repudiandae corrupti? Adipisci assumenda porro odio ullam ducimus exercitationem provident deserunt explicabo nam omnis eveniet suscipit culpa dolores, voluptate amet, nisi voluptas? Id nemo aperiam non saepe itaque!
-            </p>
-        </div>
-        
-        <div className="item-card p-4 rounded-3 bg-black mb-4 ">
-            <h3 className="mb-3 text-white">What is the purpose of jwt and how does it work?</h3>
-            <p className="text-white">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex animi maiores odit commodi necessitatibus laudantium ipsam! Cumque beatae deserunt non sed asperiores et, repellat repellendus consectetur quis, libero quibusdam similique ea, vel neque rem blanditiis soluta quaerat ab dolore possimus modi ducimus alias temporibus. Unde fuga velit delectus rem necessitatibus. Reiciendis laborum optio doloremque, repudiandae sapiente pariatur aliquam fugit distinctio obcaecati maxime eum. Illo eum quia provident nihil voluptatibus. Asperiores delectus fugit repudiandae corrupti? Adipisci assumenda porro odio ullam ducimus exercitationem provident deserunt explicabo nam omnis eveniet suscipit culpa dolores, voluptate amet, nisi voluptas? Id nemo aperiam non saepe itaque!
-            </p>
+          <h3 className="mb-3 text-white">
+            What is the purpose of JWT and how does it work?
+          </h3>
+          <p className="text-white">
+            The full form of JWT is Json Web Token. It allows clients and
+            servers to share security information. The goal of utilizing JWT is
+            to confirm the data's authenticity, not to hide it. A JWT token is
+            the combination of three parts and each part is separated by dots(.)
+            and serialized using base64. The first part is header, the middle
+            one is payload and the last part is the signature. At first JWT
+            generate an encrypted token for a valid user then we have to verify
+            the token to get any valid server request. In this way the API key
+            or the server URL can only be accessed by a valid user. The JWT
+            token is used for increase the security system of a website.
+          </p>
         </div>
       </div>
       <Footer></Footer>
