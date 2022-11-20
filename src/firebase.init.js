11285da096af1a6f53fcfcf5d99d921021b88325
ishtiyak98 <rifatishtiyak@gmail.com>
2022-11-20 20:43:21 +0600

    firebase old

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
+  apiKey: "AIzaSyC-JhW1Kj0PKC43ccsnahc0yk5m01YiutU",
+  authDomain: "furniture-house-a6e5b.firebaseapp.com",
+  projectId: "furniture-house-a6e5b",
+  storageBucket: "furniture-house-a6e5b.appspot.com",
+  messagingSenderId: "869616903012",
+  appId: "1:869616903012:web:e7c5ba917624726d7d9071"
 };
 
 // Initialize Firebase
